1beec7d708fbcdaeb93ae62f78dfd8794bc305ae
youlisse <46898546+youlisse@users.noreply.github.com>
2023-02-28 12:51:30 +0100

    yo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <artistCard/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import artistCard from './components/artist.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    artistCard
   }
 }
 </script>
